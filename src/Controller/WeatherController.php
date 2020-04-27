411f7e98cdb04782502b5e407cced2e1ec102198
Montagné Valentin <valentin.montagne@strategiacommunications.com>
2020-04-27 17:08:21 +0200

    patch faute de frappe attribut weather

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -8,7 +8,7 @@ use App\Service\WeatherService;
 
 class WeatherController extends AbstractController
 {
-    private $wheatherService;
+    private $weatherService;
 
     public function __construct(WeatherService $weather)
     {
